708644d03e745f8cf24a6cfd6fb272c48488e30f
Nazlıcan <nazliaslan725@gmail.com>
2021-02-06 17:13:34 +0300

    Odev6

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri  eklendi...");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + " " + customer.FirstName + " " + customer.LastName + " Müşterisi Silindi");
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerId + " " + customer.FirstName + " " + customer.LastName);
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,16 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-           
+            Customer customer1 = new Customer() { CustomerId = 1, FirstName = "Nazlıcan", LastName = "Aslan" };
+            Customer customer2 = new Customer() { CustomerId = 2, FirstName = "Cemal", LastName = "Aslan" };
+            Customer customer3 = new Customer() { CustomerId = 3, FirstName = "Şefika", LastName = "Aslan" };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            customerManager.List(customers);
         }
     }
 }
